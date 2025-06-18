cf5dce3e9f3aed19119d8faf5ed5a62547495070
zjcn86 <33260177+yikeshu0611@users.noreply.github.com>
2025-06-19 00:32:26 +0800

    Add files via upload

---
+++ R/install_nwR.R
@@ -0,0 +1,57 @@
+#' install
+#'
+#'
+#' @return install
+#' @export
+#'
+install_ukR <- function(){
+    e <- tryCatch(detach("package:ukR", unload = TRUE),error=function(e) 'e')
+    # check
+    (td <- tempdir(check = TRUE))
+    td2 <- '1'
+    while(td2 %in% list.files(path = td)){
+        td2 <- as.character(as.numeric(td2)+1)
+    }
+    (dest <- paste0(td,'/',td2))
+    do::formal_dir(dest)
+    dir.create(path = dest,recursive = TRUE,showWarnings = FALSE)
+    (tf <- paste0(dest,'/ukR.zip'))
+
+    if (do::is.windows()){
+        download.file(url = 'https://codeload.github.com/yikeshu0611/ukR_win/zip/refs/heads/main',
+                      destfile = tf,
+                      mode='wb')
+        unzip(zipfile = tf,exdir = dest,overwrite = TRUE)
+    }else{
+        download.file(url = 'https://codeload.github.com/yikeshu0611/ukR_mac/zip/refs/heads/main',
+                      destfile = tf,
+                      mode='wb')
+        unzip(zipfile = tf,exdir = dest,overwrite = TRUE)
+    }
+
+
+
+    if (do::is.windows()){
+        main <- paste0(dest,'/ukR_win-main')
+        (ukR <- list.files(main,'ukR_',full.names = TRUE))
+        (ukR <- ukR[do::right(ukR,3)=='zip'])
+        (k <- do::Replace0(ukR,'.*ukR_','\\.zip','\\.tgz','\\.') |> as.numeric() |> which.max())
+        unzip(ukR[k],files = 'ukR/DESCRIPTION',exdir = main)
+    }else{
+        main <- paste0(dest,'/ukR_mac-main')
+        ukR <- list.files(main,'ukR_',full.names = TRUE)
+        ukR <- ukR[do::right(ukR,3)=='tgz']
+        k <- do::Replace0(ukR,'.*ukR_','\\.zip','\\.tgz','\\.') |> as.numeric() |> which.max()
+        untar(ukR[k],files = 'ukR/DESCRIPTION',exdir = main)
+    }
+
+    (desc <- paste0(main,'/ukR'))
+    check_package(desc)
+
+    install.packages(pkgs = ukR[k],repos = NULL,quiet = FALSE)
+    message('Done(ukR)')
+    x <- suppressWarnings(file.remove(list.files(dest,recursive = TRUE,full.names = TRUE)))
+    invisible()
+}
+
+
