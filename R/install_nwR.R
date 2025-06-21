fad7598f15861ed952215de153763a3b9a8d9d46
zjcn86 <33260177+yikeshu0611@users.noreply.github.com>
2025-06-21 14:14:53 +0800

    Update install_nwR.R

--- R/install_nwR.R
+++ R/install_nwR.R
@@ -5,7 +5,6 @@
 #' @export
 #'
 install_ukR <- function(){
-    cku <- 'ukR' %in% row.names(installed.packages())
     e <- tryCatch(detach("package:ukR", unload = TRUE),error=function(e) 'e')
     e <- tryCatch(detach("package:ukR", unload = TRUE),error=function(e) 'e')
     # check
@@ -53,7 +52,7 @@ install_ukR <- function(){
     install.packages(pkgs = ukR[k],repos = NULL,quiet = FALSE)
     message('Done(ukR)')
     x <- suppressWarnings(file.remove(list.files(dest,recursive = TRUE,full.names = TRUE)))
-    if (cku) rstudioapi::sendToConsole('.rs.restartR()')
+    if ((!any(file.exists(paste0(.libPaths(),'/ukR'))))  & ("package:ukR" %in% search())) rstudioapi::sendToConsole('.rs.restartR()')
     invisible()
 }
 
